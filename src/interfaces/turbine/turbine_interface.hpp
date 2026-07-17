a40e981f467770817c8a1bc1dfb2574462978cbc
Michael Kuhn <mkuhn@nlr.gov>
2026-07-17 10:20:45 -0600

    add ability to set time step index externally

--- src/interfaces/turbine/turbine_interface.hpp
+++ src/interfaces/turbine/turbine_interface.hpp
@@ -141,6 +141,9 @@ public:
      */
     void ReadCheckpointFile(const std::string& file_path);
 
+    /// @brief Allows time step index to be set externally (e.g., for checkpoint restart)
+    void SetTimeStepIndex(int idx) {state.time_step = idx;}
+
 private:
     Model model;                    ///< Kynema-FMB class for model construction
     components::Turbine turbine;    ///< Turbine model input/output data
