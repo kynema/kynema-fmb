9e70a85a545356b5b3caaa18aee716cd74b51872
Jon Rood <jon.rood@nrel.gov>
2026-07-28 13:16:35 -0600

    Update warning regex.

--- tests/CTestCustom.cmake
+++ tests/CTestCustom.cmake
@@ -1,8 +1,8 @@
 set(CTEST_CUSTOM_WARNING_EXCEPTION ".*: warning: could not create compact unwind*"
                                    ".*has no symbols*"
                                    ".*submake: resetting jobserver mode*"
-                                   "warning #20014-D"  # calling a __host__ function from a __host__ __device__ function
-                                   "warning #20015-D"  # related host/device call diagnostic
+                                   ".*[Ww]arning #20014-D.*"  # calling a __host__ function from a __host__ __device__ function
+                                   ".*[Ww]arning #20015-D.*"  # related host/device call diagnostic
 )
 
 set(CTEST_CUSTOM_MAXIMUM_FAILED_TEST_OUTPUT_SIZE "131072")
