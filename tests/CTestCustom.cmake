16ec8a7cd8a12e5de96f6ef0c1d5a91615ba3bfa
Jon Rood <jon.rood@nrel.gov>
2026-07-28 13:11:19 -0600

    Ignore host device function warnings in CDash.

--- tests/CTestCustom.cmake
+++ tests/CTestCustom.cmake
@@ -1,5 +1,9 @@
 set(CTEST_CUSTOM_WARNING_EXCEPTION ".*: warning: could not create compact unwind*"
                                    ".*has no symbols*"
-                                   ".*submake: resetting jobserver mode*")
+                                   ".*submake: resetting jobserver mode*"
+                                   "warning #20014-D"  # calling a __host__ function from a __host__ __device__ function
+                                   "warning #20015-D"  # related host/device call diagnostic
+)
+
 set(CTEST_CUSTOM_MAXIMUM_FAILED_TEST_OUTPUT_SIZE "131072")
 set(CTEST_CUSTOM_MAXIMUM_PASSED_TEST_OUTPUT_SIZE "131072")
