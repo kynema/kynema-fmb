71f63e987db7e8bc44590ee74443444083b97966
Justin Porter <Justin.Porter@nlr.gov>
2026-09-10 13:40:06 -0600

    Clean up of damping tests. Passing at 1e-3 relative tolerance, feel like it should work (does not) at tighter 1e-6 tol.

--- tests/regression_tests/regression/test_beam_damping.cpp
+++ tests/regression_tests/regression/test_beam_damping.cpp
@@ -49,14 +49,16 @@ TEST(DynamicBeamTest, Damping) {
     };
 
     // Stiffness matrix for uniform composite beam section
+    // Made diagonal so can test directional mu.
     constexpr auto stiffness_matrix = std::array{
         std::array{1368.17e3, 0., 0., 0., 0., 0.},
         std::array{0., 88.56e3, 0., 0., 0., 0.},
         std::array{0., 0., 38.78e3, 0., 0., 0.},
-        std::array{0., 0., 0., 16.9600e3, 17.6100e3, -0.3510e3},
-        std::array{0., 0., 0., 17.6100e3, 59.1200e3, -0.3700e3},
-        std::array{0., 0., 0., -0.3510e3, -0.3700e3, 141.470e3},
+        std::array{0., 0., 0., 16.9600e3, 0., 0.},
+        std::array{0., 0., 0., 0., 59.1200e3, 0.},
+        std::array{0., 0., 0., 0., 0., 141.470e3},
     };
+    
 
     // Node locations (GLL quadrature)
     const auto num_nodes = 5UL;
@@ -79,7 +81,6 @@ TEST(DynamicBeamTest, Damping) {
             .Build();
     });
 
-    const double scalar_mu(0.0001);  // 1/s
     const auto array_mu = std::array{0.0001, 0.0004, 0.0002,
                                      0.0003, 0.0002, 0.0004}; // 1/s
 
@@ -110,7 +111,7 @@ TEST(DynamicBeamTest, Damping) {
     const bool is_dynamic_solve(true);
     const size_t max_iter(5);
     // step_size and num_steps are updated below once the mode's natural frequency is known.
-    constexpr size_t steps_per_cycle = 128;
+    constexpr size_t steps_per_cycle = 1024;
     const double num_cycles = 1.5;
     double step_size(0.0001);  // seconds
     const double rho_inf(1.0);
@@ -126,27 +127,10 @@ TEST(DynamicBeamTest, Damping) {
     // // Get ID of last node
     // const auto last_node_id = beam_node_ids.back();
 
-    // Ideally would pull from an eigenanalysis to get the mode shapes
-    // and initial velocity, just prototyping here.
-    // (x/L)^3 is a rough approx of first bending mode,
-    // technically need rotational velocity as well
-
-    /*
-    OLD Initialization
-    // Start the beam with an initial velocity in the first component
-    for (size_t i = 0; i < beam_node_ids.size(); ++i) {
-        auto node_v = Kokkos::subview(state.v, beam_node_ids[i], Kokkos::ALL);
-        node_v(0) = node_s[i] * node_s[i] * node_s[i];
-        node_v(1) = 0.;
-        node_v(2) = 0.;
-        node_v(3) = 0.;
-        node_v(4) = 0.;
-        node_v(5) = 0.;
-    }
-    */
-
-
     // Mode to use for the initial velocity shape.
+    // TODO: Maybe the computation needs to be on the velocity for the torsion mode
+    // Otherwise, have quaternions for positions and that could easily become a problem.
+    // mode_ind = 3 was an issue re that.
     const size_t mode_ind = 0;
 
     // Extract the system matrices at the initial (undeformed) state so we can
@@ -262,12 +246,15 @@ TEST(DynamicBeamTest, Damping) {
 
 
     // 3 translations + 4 quaternions per node
-    const auto tip_displacement_col = 1 + dir_ind + 7*(beam_node_ids.size() - 1);
+    auto tip_displacement_col = 1 + dir_ind + 7*(beam_node_ids.size() - 1);
+    if (dir_ind >= 3) {
+        // need to go past the first entry of the quaternion if it is a rotation.
+        tip_displacement_col++;
+    }
 
     std::vector<size_t> peak_inds;
-    // Assume that should have at least 50 steps per cycle
-    // for sizing the memory (100 is rule of thumb to be accurate)
-    peak_inds.reserve(num_steps / 50);
+    // Only simulating two peaks
+    peak_inds.reserve(2);
 
     for (size_t i = 1; i + 1 < displacement_history.size(); ++i) {
         const auto prev = displacement_history[i - 1][tip_displacement_col];
@@ -279,8 +266,9 @@ TEST(DynamicBeamTest, Damping) {
         }
     }
 
-    // Need at least two peaks for log decrement
-    ASSERT_GE(peak_inds.size(), 2U);
+    // Should have exactly two peaks by doing 1.5 cycles
+    // starting towards the positive direction.
+    ASSERT_EQ(peak_inds.size(), 2U);
 
     std::vector<double> peak_vals;
     peak_vals.reserve(peak_inds.size());
@@ -294,40 +282,23 @@ TEST(DynamicBeamTest, Damping) {
         peak_times.push_back(displacement_history[peak_ind][0]);
     }
 
-    std::vector<double> zeta;
-    zeta.reserve(peak_vals.size() - 1);
-
-    std::vector<double> omega_n;
-    omega_n.reserve(peak_vals.size() - 1);
-
     constexpr auto two_pi = 2. * std::numbers::pi;
-    
-    for (size_t i = 0; i + 1 < peak_vals.size(); ++i) {
 
-        const auto delta_i = std::log(peak_vals[i] / peak_vals[i + 1]);
-        
-        const auto zeta_i = delta_i / std::sqrt(two_pi * two_pi +
-                                    delta_i * delta_i);
+    const auto delta = std::log(peak_vals[0] / peak_vals[1]);
+    const auto zeta = delta / std::sqrt(two_pi * two_pi + delta * delta);
+    const auto omega_n = (two_pi / (peak_times[1] - peak_times[0]))
+                         / std::sqrt(1. - zeta * zeta);
 
-        const auto omega_n_i = (two_pi / (peak_times[i + 1] - peak_times[i]))
-                        / std::sqrt(1. - zeta_i * zeta_i);
+    ASSERT_NEAR(zeta, array_mu[dir_ind] * omega_n / 2.0, 1e-3 * zeta);
 
-        zeta.push_back(zeta_i);
-        omega_n.push_back(omega_n_i);
-    }
+    //omega_n is computed natural frequency, omega is from eigensolution.
+    ASSERT_NEAR(omega_n, omega, 1e-3 * omega);
 
-    // Check only the last few damping values starting from the end
-    // The initialization is imperfect, so the early ones have other transients
-    // If above gets updated to an eigensolution for the initialization
-    // then should be able to run fewer cycles and immediately evaluate
-    // the damping and at tighter tolerance.
-    for (size_t eval_count = 0, idx = zeta.size() - 1;
-            eval_count < 3 && idx > 4;
-            ++eval_count, --idx) 
-    {
-        // Needs to actually refer to the array instead of the scalar here.
-        ASSERT_NEAR(zeta[idx], scalar_mu * omega_n[idx] / 2.0, 1e-3*zeta[idx]);
-    }
+
+    ASSERT_NEAR(zeta, array_mu[dir_ind] * omega_n / 2.0, 1e-6 * zeta);
+
+    //omega_n is computed natural frequency, omega is from eigensolution.
+    ASSERT_NEAR(omega_n, omega, 1e-6 * omega);
 }
 
 }  // namespace kynema_fmb::tests
