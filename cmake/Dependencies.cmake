923b05862dc7cc97dc0cd2f3d22d10a4eac98311
Jon Rood <jon.rood@nrel.gov>
2026-09-15 13:10:36 -0600

    Lowercase superlu.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -14,7 +14,7 @@ function(kynema_fmb_setup_dependencies)
     # Sparse Direct Linear Solvers
     #----------------------------------------
     if(KYNEMA_FMB_ENABLE_SUPERLU)
-        find_package(SuperLU REQUIRED)
+        find_package(superlu REQUIRED)
     endif()
 
     if(KYNEMA_FMB_ENABLE_SUPERLU_MT)
